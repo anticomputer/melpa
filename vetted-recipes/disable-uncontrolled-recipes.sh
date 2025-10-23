3cd440c1daca1b3dbcb6790f8327525ee8740942
Bas Alberts <bas@anti.computer>
2025-10-22 21:19:23 -0400

    moving to a simplified fork system for my melpa

--- vetted-recipes/disable-uncontrolled-recipes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# disable any recipes that don't point to controlled repos
-grep -L anticomputer * | xargs -i sed -i -e 's/^/;;/' '{}'
-# note: we don't sed ourselves by virtue of anticomputer ;)
